23ddef574543170ca62d83a2ecc2c08a76109757
Diceking1208 <phy5840@naver.com>
2023-05-10 23:19:16 +0900

    diceking,taeduri

--- lecture10/main.c
+++ lecture10/main.c
@@ -4,7 +4,7 @@
 int main()
 {
 
-	char screen[500];/*가로 : 30 세로 : 15 */
+	char screen[5000];
 	
 	int i = 0;
 	while(i<450)
@@ -12,16 +12,34 @@ int main()
 		screen[i]='0';
 		i=i+1;
 	}
-	screen[30]='\0';
+	
 
 	int a = 0;
 	while(a<15)
 	{
+		int b = 0;
+		while(b<30)
+		{
+			if(a==0||a==14)
+			{
+				screen[b]='*';
+			}
+			else if(b==0||b==29)
+			{
+				screen[b]='*';
+			}
+			else
+			{
+				screen[b]=' ';
+			}
+			b=b+1;
+		}	
 		screen[30]='\0';
 		a=a+1;
 		printf("%s\n",screen);
 	}
 	
+
 	return 0;
 }
 
