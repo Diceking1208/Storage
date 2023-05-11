6edb45ea5ea36704201c18e6d38f9dbbe88c3417
Diceking1208 <phy5840@naver.com>
2023-05-12 00:59:04 +0900

    diceking, lecture10

--- lecture10/main.c
+++ lecture10/main.c
@@ -1,43 +1,8 @@
 #include <stdio.h>
+#include "screen.h"
 
 int main()
 {
-
-	char screen[5000];
-	
-	int i = 0;
-	while(i<450)
-	{
-		i=i+1;
-	}
-	
-	int a = 0;
-	while(a<15)
-	{
-		int b = 0;
-		while(b<30)
-		{
-			if(a==0||a==14)
-			{
-				screen[b]='*';
-			}
-			else if(b==0||b==29)
-			{
-				screen[b]='*';
-			}
-			else
-			{
-				screen[b]=' ';
-			}
-			b=b+1;
-		}
-
-		screen[30]='\0';
-		a=a+1;
-		printf("%s\n",screen);
-	}
-	
-
-	return 0;
+	title();
+        return 0;
 }
-
